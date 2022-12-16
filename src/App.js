cb0dd90c49844db65209d9bf28616a44379ec0ed
psi <psi@ps-lin-lp-200.thepsi.com>
2022-12-16 10:52:55 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import Shell from './components/Shell'
 import Login from './container/Login'
 import Dashboard from './container/Dashboard'
 import ErrorPage from './container/ErrorPage'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <>
-    <Shell>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </Router>
-    </Shell>
-    <ToastContainer/>
+      <Shell>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </Router>
+      </Shell>
+      <ToastContainer />
     </>
 
   );
